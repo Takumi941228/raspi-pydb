64f90e2c28b54d25a0f9fc0752f945b3ed7eebf4
Takumi Hirota <hirota.takumi.uitec@gmail.com>
2024-09-19 19:34:18 +0900

    codesを追加

---
+++ codes/py/db_ambient.py
@@ -0,0 +1,39 @@
+#coding: utf-8
+
+#モジュールをインポート
+import pymysql.cursors #PythonからDBを取扱う
+
+#DB への接続情報
+DB_USER = 'iot_user'
+DB_PASS = 'Passw0rd'
+DB_HOST = 'localhost'
+DB_NAME = 'iot_storage'
+
+#共通で使うオブジェクトを指すための準備
+sql_connection = None
+
+def connect():
+    global sql_connection
+
+    #DB サーバに接続する
+    sql_connection = pymysql.connect(
+        user = DB_USER, #データベースにログインするユーザ名
+        passwd = DB_PASS,#データベースユーザのパスワード
+        host = DB_HOST, #接続先DBのホストorIPアドレス
+        db = DB_NAME
+    )
+
+def insert_row(row): #クエリの作成
+    query = "INSERT INTO Ambient(timestamp, identifier, temperature, humidity, pressure) " \
+        " VALUES(%(timestamp)s, %(identifier)s, %(temperature)s, %(humidity)s, %(pressure)s)";
+
+    #cursor オブジェクトのインスタンスを生成
+    sql_cursor = sql_connection.cursor()
+
+    #クエリを実行する
+    result = sql_cursor.execute(query, row)
+
+    #変更を実際に反映させる
+    sql_connection.commit()
+
+    return(result)
